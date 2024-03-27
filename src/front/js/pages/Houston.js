9f481902edc9db108355dd0ec6e2004adc1eb56e
Jaemarlee Goin <150928570+Jaemarlee@users.noreply.github.com>
2024-03-27 19:31:48 +0000

    Houston restaurants added

--- src/front/js/pages/Houston.js
+++ src/front/js/pages/Houston.js
@@ -5,7 +5,69 @@ export const Houston = () => {
 
 	return (
 		<div className="container">
-            <h1>Houston</h1>
+            <div class="restaurant">
+        <h2>Green Seed Vegan</h2>
+        <p>Address: 4320 Almeda Rd, Houston, TX 77004</p>
+        <p>Phone: (832) 582-5566</p>
+        <a href="https://www.google.com/maps/search/?api=1&query=Green+Seed+Vegan+Houston+TX" target="_blank">Directions</a>
+    </div>
+    <div class="restaurant">
+        <h2>Veegos Vegan Eatery</h2>
+        <p>Address: 2435 Times Blvd, Houston, TX 77005</p>
+        <p>Phone: (713) 489-5323</p>
+        <a href="https://www.google.com/maps/search/?api=1&query=Veegos+Vegan+Eatery+Houston+TX" target="_blank">Directions</a>
+    </div>
+    <div class="restaurant">
+        <h2>Baba Yega Cafe</h2>
+        <p>Address: 2607 Grant St, Houston, TX 77006</p>
+        <p>Phone: (713) 522-0042</p>
+        <a href="https://www.google.com/maps/search/?api=1&query=Baba+Yega+Cafe+Houston+TX" target="_blank">Directions</a>
+    </div>
+    <div class="restaurant">
+        <h2>Cafe TH</h2>
+        <p>Address: 2108 Pease St, Houston, TX 77003</p>
+        <p>Phone: (713) 225-4766</p>
+        <a href="https://www.google.com/maps/search/?api=1&query=Cafe+TH+Houston+TX" target="_blank">Directions</a>
+    </div>
+    <div class="restaurant">
+        <h2>Ripe Cuisine</h2>
+        <p>Address: 2518 Bissonnet St, Houston, TX 77005</p>
+        <p>Phone: (713) 637-4316</p>
+        <a href="https://www.google.com/maps/search/?api=1&query=Ripe+Cuisine+Houston+TX" target="_blank">Directions</a>
+    </div>
+    <br></br>
+    <div class="restaurant">
+        <h2>Pepper Tree Veggie Cuisine</h2>
+        <p>Address: 3821 Richmond Ave, Houston, TX 77027</p>
+        <p>Phone: (713) 621-9488</p>
+        <a href="https://www.google.com/maps/search/?api=1&query=Pepper+Tree+Veggie+Cuisine+Houston+TX" target="_blank">Directions</a>
+    </div>
+    <div class="restaurant">
+        <h2>Field of Greens</h2>
+        <p>Address: 2320 W Alabama St, Houston, TX 77098</p>
+        <p>Phone: (713) 533-0029</p>
+        <a href="https://www.google.com/maps/search/?api=1&query=Field+of+Greens+Houston+TX" target="_blank">Directions</a>
+    </div>
+    <div class="restaurant">
+        <h2>Soul Food Vegan</h2>
+        <p>Address: 2901 Emancipation Ave, Houston, TX 77004</p>
+        <p>Phone: (832) 831-2856</p>
+        <a href="https://www.google.com/maps/search/?api=1&query=Soul+Food+Vegan+Houston+TX" target="_blank">Directions</a>
+    </div>
+    <div class="restaurant">
+        <h2>Sweet Veggie</h2>
+        <p>Address: 9834 Jones Rd, Houston, TX 77065</p>
+        <p>Phone: (832) 604-6580</p>
+        <a href="https://www.google.com/maps/search/?api=1&query=Sweet+Veggie+Houston+TX" target="_blank">Directions</a>
+    </div>
+    <div class="restaurant">
+        <h2>Pat Greer's Kitchen</h2>
+        <p>Address: 412 W Clay St, Houston, TX 77019</p>
+        <p>Phone: (713) 807-0101</p>
+        <a href="https://www.google.com/maps/search/?api=1&query=Pat+Greer%27s+Kitchen+Houston+TX" target="_blank">Directions</a>
+    </div>
+            
+            
         </div>
     );
 };
