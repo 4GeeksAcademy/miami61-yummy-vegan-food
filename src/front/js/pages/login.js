238a38b1a10dda90acf76fcf30d529097cec94bc
addmy01 <70408689+addmy01@users.noreply.github.com>
2024-04-04 23:07:45 +0000

    registration start

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,37 +1,52 @@
 import React from "react";
+import { userRef, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
-import opt1Logo from "../../img/opt1Logo.png";
-
 
 
 export const Login = () => {
-	return (
-		<div className="container mb-auto">
-			<form className="mb-auto">
+	const userRef = useRef();
+	const errRef = useRef();
 
+	const [user, setUser] = useState('');
+	const [pwd, setPwd] = useState('');
+	const [errMsg, setErrMsg] = useState('');
+	const [success, setSuccess] = useState(false);
+
+	useEffect(() => {
+		userRef.current.focus();
+	}, [])
+
+	useEffect(() => {
+		setErrMsg('');
+	}, [user, pwd])
+
+	return (
+		<section className="container mb-auto">
+			<p ref={errRef} className={errMsg ? "errmsg" : "offscreen" } aria-live="assertive">{errMsg}</p>
+			<form className="mb-auto">
+					<h1 className="account align-items-center">Log In</h1>
 				<div className="mb-3">
-					<label className="form-label" htmlFor="email">email</label>
-					<input className="form-control" id="email" type="email" placeholder="email"/>
+					<label className="form-label" htmlFor="email">Email</label>
+					<input className="form-control" id="email" type="email" placeholder="email" />
 				</div>
 				<div className="mb-3">
 					<label className="form-label" htmlFor="password">Password</label>
-					<input className="form-control" id="password" type="password" placeholder="password"/>
+					<input className="form-control" id="password" type="password" placeholder="password" />
 				</div>
 
 				<div class="form-check">
-					<input class="form-check-input " type="checkbox" value="" id="flexCheckChecked"/>
+					<input class="form-check-input " type="checkbox" value="" id="flexCheckChecked" />
 					<label class="form-check-label" for="flexCheckChecked">
 						Remember me
 					</label>
 				</div>
-				
+
 				<div class="col-auto d-flex gap-5 align-items-center">
-					<button type="submit" className="btn submitbtn mb-3">Submit</button>				
+					<button type="submit" className="btn submitbtn mb-3">Submit</button>
 					<button type="submit" className="btn submitbtn mb-3">Forget Password</button>
 				</div>
 				<Link to="/registration" className="account">Create new account</Link>
 			</form>
-		</div>
+		</section>
 	);
 };
--- src/front/js/pages/registration.js
+++ src/front/js/pages/registration.js
@@ -1,32 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import opt1Logo from "../../img/opt1Logo.png";
 
 
 
 export const Registration = () => {
+	const[ errMsg, setErrMsg]= useState(null) 
+	function handleSubmit(event){
+		event.preventDefault()
+		fetch(process.env.BACKEND_URL + "/register", {
+			method:"POST", 
+			headers:{'Content-Type':"application/json"},
+			mode:"cors",
+			body:{
+				username:event.target.name.value,
+				email:event.target.email.value,
+				password:event.target.password.value
+				}
+		}).then(response => {
+			if (response.status == 400){
+				throw new Error(response.json().message)
+			}
+			return response.json()
+		}).then(result => {
+			console.log(result)
+		}).catch(error => {
+			setErrMsg(error.message)
+		})
+	}
 	return (
 		<div className="container">
-			<form className="mb-auto">
-
+			<form className="mb-auto" onSubmit={handleSubmit}>				
 				<div className="mb-3">
-					<label className="form-label" htmlFor="name">Name</label>
-					<input className="form-control" id="name" type="text" placeholder="name"/>
+					<label className="form-label" htmlFor="name" >Name</label>
+					<input className="form-control" id="name" type="text" placeholder="name"  name="name"/>
 				</div>
 				<div className="mb-3">
-					<label className="form-label" htmlFor="email">email</label>
-					<input className="form-control" id="email" type="email" placeholder="email"/>
+					<label className="form-label" htmlFor="email">Email</label>
+					<input className="form-control" id="email" type="email" placeholder="email" name="email"/>
 				</div>
 				<div className="mb-3">
 					<label className="form-label" htmlFor="password">Password</label>
-					<input className="form-control" id="password" type="password" placeholder="password"/>
+					<input className="form-control" id="password" type="password" placeholder="password" name="password"/>
 				</div>
 				<div className="mb-3">
 					<label className="form-label" htmlFor="confirm_password">Confirm Password</label>
-					<input className="form-control" id="confirm_password" type="password" placeholder="please confirm your password"/>
+					<input className="form-control" id="confirm_password" type="password" placeholder="please confirm your password" name="confirm_password"/>
 				</div>
-				
+				{errMsg && <div class="alert alert-danger" role="alert">{errMsg}</div>}
 				<div class="col-auto">
 					<button type="submit" className="btn submitbtn mb-3">Submit</button>					
 				</div>
