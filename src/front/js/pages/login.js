0eb07e5db93a843a57e19d4ade582fff4c30543f
addmy01 <70408689+addmy01@users.noreply.github.com>
2024-04-08 22:38:53 +0000

    working on registration

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -6,83 +6,92 @@ export const Login = () => {
 	// const [user, setUser] = useState('');
 	// const [pwd, setPwd] = useState('');
 	// const [success, setSuccess] = useState(false);
-    const [credentials, setCredentials] = useState({ email: '', password: '' });
-    const [errMsg, setErrMsg] = useState('');
-    const navigate = useNavigate();
+	const [credentials, setCredentials] = useState({ email: '', password: '' });
+	const [errMsg, setErrMsg] = useState('');
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		setErrMsg('');
 	}, [credentials.email, credentials.password])
 
 	function handleSubmit(event) {
-    event.preventDefault();
-    // const email = event.target.email.value;
-    // const password = event.target.password.value;
-    const {email, password} =credentials;
+		event.preventDefault();
+		// const email = event.target.email.value;
+		// const password = event.target.password.value;
+		const { email, password } = credentials;
 
-    // Check if email and password are provided
-    if (email && password) {
-        // Make a request to check if email and password are in the database
-        fetch(process.env.BACKEND_URL + "api/login", {
-            method: "POST",
-            headers: { 'Content-Type': "application/json" },
-            body: JSON.stringify({
-                email: email,
-                password: password
-            })
-        }).then(response => {
-            if (response.status === 200) {
-                // Email and password are valid, navigate to main tab
-                navigate('/');
-            } else if (response.status === 400) {
-                // Email or password are incorrect
-                return response.json().then(data => {
-                    throw new Error(data.message || "Incorrect email or password");
-                });
-            } else {
-                // Other server errors
-                throw new Error("Something went wrong with the server.");
-            }
-        }).catch(error => {
-            // Handle errors
-            // setSucMsg(null);
-            setErrMsg(error.message);
-        });
-    } else {
-        // Email or password is missing
-        setErrMsg("Please enter both email and password.");
-    }
-}
+		// Check if email and password are provided
+		if (email && password) {
+			// Make a request to check if email and password are in the database
+			fetch(process.env.BACKEND_URL + "api/login", {
+				method: "POST",
+				headers: { 'Content-Type': "application/json" },
+				body: JSON.stringify({
+					email: email,
+					password: password
+				})
+			}).then(response => {
+				if (response.status === 200) {
+					// Email and password are valid, navigate to main tab
+					navigate('/');
+				} else if (response.status === 400) {
+					// Email or password are incorrect
+					return response.json().then(data => {
+						throw new Error(data.message || "Incorrect email or password");
+					});
+				} else {
+					// Other server errors
+					throw new Error("Something went wrong with the server.");
+				}
+			}).catch(error => {
+				// Handle errors
+				// setSucMsg(null);
+				setErrMsg(error.message);
+			});
+		} else {
+			// Email or password is missing
+			setErrMsg("Please enter both email and password.");
+		}
+	}
 
-	function handleChange(e){
-        setCredentials({... credentials, [e.target.id]: e.target.value});
-    }
+	function handleChange(e) {
+		setCredentials({ ...credentials, [e.target.id]: e.target.value });
+	}
 	return (
-		<section className="container mb-auto">			
-			<form className="mb-auto" onSubmit={handleSubmit}>
-					<h1 className="account align-items-center">Log In</h1>
-				<div className="mb-3">
-					<label className="form-label" htmlFor="email">email</label>
-					<input className="form-control" id="email" type="email" placeholder="email" value={credentials.email} onChange={handleChange}/>
+		<section className="container mb-auto">
+			<div className="container row">
+				<div className="align-self-center">
+					<h1>Login</h1>
 				</div>
-				<div className="mb-3">
-					<label className="form-label" htmlFor="password">Password</label>
-					<input className="form-control" id="password" type="password" placeholder="password" value={credentials.password} onChange={handleChange}/>
-				</div>
+				<div className="col-md-12 col-lg-6 align-self-center">
+					<h1>Welcome to Yummy vegan food</h1></div>
+				<div className="col-md-12 col-lg-6">
+					<form className="mb-auto" onSubmit={handleSubmit}>
+						<h1 className="account align-items-center">Log In</h1>
+						<div className="mb-3">
+							<label className="form-label" htmlFor="email">email</label>
+							<input className="form-control" id="email" type="email" placeholder="email" value={credentials.email} onChange={handleChange} />
+						</div>
+						<div className="mb-3">
+							<label className="form-label" htmlFor="password">Password</label>
+							<input className="form-control" id="password" type="password" placeholder="password" value={credentials.password} onChange={handleChange} />
+						</div>
 
-				<div className="form-check">
-					<input className="form-check-input " type="checkbox" value="" id="flexCheckChecked" />
-					<label className="form-check-label" htmlFor="flexCheckChecked">
-						Remember me
-					</label>
-				</div>
+						<div className="form-check">
+							<input className="form-check-input " type="checkbox" value="" id="flexCheckChecked" />
+							<label className="form-check-label" htmlFor="flexCheckChecked">
+								Remember me
+							</label>
+						</div>
 
-				<div className="col-auto d-flex gap-5 align-items-center">
-					<button type="submit" className="btn submitbtn mb-3">Submit</button>				
-					<button type="submit" className="btn submitbtn mb-3">Forget Password</button>
+						<div className="col-auto d-flex gap-5 align-items-center">
+							<button type="submit" className="btn submitbtn mb-3">Submit</button>
+							<button type="submit" className="btn submitbtn mb-3">Forget Password</button>
+						</div>
+						<Link to="/registration" className="account">Create new account</Link>
+					</form>
 				</div>
-				<Link to="/registration" className="account">Create new account</Link>
-			</form>
+			</div>
 		</section>
 	);
 };
--- src/front/js/pages/registration.js
+++ src/front/js/pages/registration.js
@@ -2,24 +2,24 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 export const Registration = () => {
-	const[ errMsg, setErrMsg]= useState(null) 
-	const[ sucMsg, setSucMsg]= useState(null)
+	const [errMsg, setErrMsg] = useState(null)
+	const [sucMsg, setSucMsg] = useState(null)
 	const navigate = useNavigate()
 
 
-	function handleSubmit(event){
+	function handleSubmit(event) {
 		event.preventDefault()
-		if (event.target.password.value === event.target.confirm_password.value){
+		if (event.target.password.value === event.target.confirm_password.value) {
 			fetch(process.env.BACKEND_URL + "/api/register", {
-				method:"POST", 
-				headers:{'Content-Type':"application/json"},
-				body:JSON.stringify({
-					username:event.target.name.value,
-					email:event.target.email.value,
-					password:event.target.password.value
-					})
+				method: "POST",
+				headers: { 'Content-Type': "application/json" },
+				body: JSON.stringify({
+					username: event.target.name.value,
+					email: event.target.email.value,
+					password: event.target.password.value
+				})
 			}).then(response => {
-				if (response.status == 400){
+				if (response.status == 400) {
 					return response.json().then(data => {
 						throw new Error(data.message || "Something went wrong");
 					});
@@ -30,42 +30,52 @@ export const Registration = () => {
 				setSucMsg(result.message)
 				setTimeout(() => {
 					navigate('/login')
-				}, 2000)			
+				}, 2000)
 			}).catch(error => {
 				setSucMsg(null)
 				setErrMsg(error.message)
 			})
-		}else{
+		} else {
 			setErrMsg("password does not match")
 		}
-		
+
 	}
 	return (
-		<div className="container">
-			<form className="mb-auto" onSubmit={handleSubmit}>				
-				<div className="mb-3">
-					<label className="form-label" htmlFor="name" >Name</label>
-					<input className="form-control" id="name" type="text" placeholder="name"  name="name"/>
+		<div className="container row">
+			<div className="row align-self-center">
+				<h1>Registration</h1>
+			</div>
+			<div className="col-md-12 col-lg-6 align-self-center">
+				<h1>Welcome to Yummy vegan food</h1>
+			</div>
+			<div className="col-md-12 col-lg-6">
+				<div className="container">
+					<form className="mb-auto" onSubmit={handleSubmit}>
+						<div className="mb-3">
+							<label className="form-label" htmlFor="name" >Name</label>
+							<input className="form-control" id="name" type="text" placeholder="name" name="name" />
+						</div>
+						<div className="mb-3">
+							<label className="form-label" htmlFor="email">Email</label>
+							<input className="form-control" id="email" type="email" placeholder="email" name="email" />
+						</div>
+						<div className="mb-3">
+							<label className="form-label" htmlFor="password">Password</label>
+							<input className="form-control" id="password" type="password" placeholder="password" name="password" />
+						</div>
+						<div className="mb-3">
+							<label className="form-label" htmlFor="confirm_password">Confirm Password</label>
+							<input className="form-control" id="confirm_password" type="password" placeholder="please confirm your password" name="confirm_password" />
+						</div>
+						{errMsg && <div class="alert alert-danger" role="alert">{errMsg}</div>}
+						{sucMsg && <div class="alert alert-success" role="alert">{sucMsg}</div>}
+						<div class="col-auto">
+							<button type="submit" className="btn submitbtn mb-3">Submit</button>
+						</div>
+						<Link to="/login" className="account">Already have account</Link>
+					</form>
 				</div>
-				<div className="mb-3">
-					<label className="form-label" htmlFor="email">Email</label>
-					<input className="form-control" id="email" type="email" placeholder="email" name="email"/>
-				</div>
-				<div className="mb-3">
-					<label className="form-label" htmlFor="password">Password</label>
-					<input className="form-control" id="password" type="password" placeholder="password" name="password"/>
-				</div>
-				<div className="mb-3">
-					<label className="form-label" htmlFor="confirm_password">Confirm Password</label>
-					<input className="form-control" id="confirm_password" type="password" placeholder="please confirm your password" name="confirm_password"/>
-				</div>
-				{errMsg && <div class="alert alert-danger" role="alert">{errMsg}</div>}
-				{sucMsg && <div class="alert alert-success" role="alert">{sucMsg}</div>}
-				<div class="col-auto">
-					<button type="submit" className="btn submitbtn mb-3">Submit</button>					
-				</div>
-				<Link to="/login" className="account">Already have account</Link>
-			</form>
+			</div>
 		</div>
 	);
 };
