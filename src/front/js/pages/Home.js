166f78439e887ce882ecf66043060526845096e2
Legend Andrews <139176918+Cantrell24@users.noreply.github.com>
2024-04-09 20:24:43 +0000

    Home Page Changes

--- src/front/js/pages/Home.js
+++ src/front/js/pages/Home.js
@@ -4,24 +4,133 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 
+
 export const Home = () => {
 	const { store, actions } = useContext(Context);
-
+	
+	
 	return (
-		<div className="text-center mt-5 homeDiv">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
+		<div className="text-center mt-5 homeDiv body">
+			<h1>Vegan Dishes in your city!</h1>
+		
+			
 			<p>
 				This boilerplate comes with lots of documentation:{" "}
 				<a href="https://start.4geeksacademy.com/starters/react-flask">
 					Read documentation
 				</a>
 			</p>
+
+			<div className="alert alert-info">
+				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+			</div>
+			
+			<body>
+				<div className="background">Welcome
+				<div className="container-welcome">Lets eat some chicken</div>
+
+
+				</div>
+
+				<div className="div1">New York
+				<div className="container"><p>New York's status as a cultural melting pot fosters a rich tapestry of culinary influences, 
+						inspiring chefs to craft inventive and delicious plant-based dishes. 
+						The city's sheer size and density create a competitive environment, 
+						driving vegan restaurants to constantly innovate and elevate their offerings. 
+						Moreover, New Yorkers are increasingly health-conscious and environmentally aware, 
+						ueling the demand for vegan options and ensuring a thriving vegan dining scene.</p></div>
+						
+				</div>
+
+				
+					
+
+				<div className="div2">Los Angeles
+				<div className="containerla"><p>Los Angeles stands out as a premier destination for vegan dining due to its abundant
+					 access to fresh, locally sourced produce year-round. The city's diverse population and cultural influence 
+					 create a dynamic culinary landscape, inspiring chefs to push boundaries and craft innovative plant-based 
+					 dishes. Additionally, LA's health-conscious and eco-friendly ethos aligns perfectly with the principles of 
+					 veganism, driving a growing demand for vegan options across the city's thriving restaurant scene. With a 
+					 plethora of vegan eateries ranging from casual cafes to upscale dining establishments, Los Angeles offers 
+					 something to suit every palate and preference, solidifying its reputation as a haven for vegan food 
+					 enthusiasts.</p></div>
+
+					 <a
+				href="https://silver-succotash-g4q4wq49j57q39jqp-3000.app.github.dev/la"
+				target=""
+				rel="noopener noreferrer"><img onMouseEnter={handleOnHover} 
+				src="https://townmapsusa.com/images/maps/map_of_los_angeles_ca.jpg" width="250" height="500" 
+				className="me-5 cityimage"/></a>
+				</div>
+
+		
+
+				<div className="div3">Houston
+				<div className="container"><p>Houston's emergence as a top destination for vegan dining is fueled by several 
+					factors. Firstly, the city's diverse and multicultural population inspires a rich tapestry of culinary 
+					influences, resulting in a wide variety of flavorful vegan options that draw from global cuisines. 
+					Additionally, Houston's strong emphasis on sustainability and environmental consciousness aligns well 
+					with the ethos of veganism, leading to a growing demand for plant-based dining options. The city's vibrant food scene continually pushes
+					 boundaries, resulting in creative and delicious plant-based dishes that cater to both vegans and 
+					 non-vegans alike.</p>
+				</div>
+
+				<a href="https://silver-succotash-g4q4wq49j57q39jqp-3000.app.github.dev/houston"
+						target=""
+						rel="noopener noreferrer"><img onMouseEnter={handleOnHover}  
+						src="https://townmapsusa.com/images/maps/map_of_houston_tx.jpg" width="250" height="500"
+						className="me-5 cityimage houstonstate"/></a>
+
+				<section>
+				<a 
+				onMouseOut={handleOffHover}
+				href="https://silver-succotash-g4q4wq49j57q39jqp-3000.app.github.dev/nyc"
+				target=""
+				rel="noopener noreferrer"><img onMouseEnter={handleOnHover}
+				src="https://townmapsusa.com/images/maps/map_of_new_york_ny.jpg" width="250" height="500"
+				className="me-5 cityimage"/></a> 
+			
+				<a
+				href="https://silver-succotash-g4q4wq49j57q39jqp-3000.app.github.dev/la"
+				target=""
+				rel="noopener noreferrer"><img onMouseEnter={handleOnHover} 
+				src="https://townmapsusa.com/images/maps/map_of_los_angeles_ca.jpg" width="250" height="500" 
+				className="me-5 cityimage"/></a>
+				
+				<a
+				href="https://silver-succotash-g4q4wq49j57q39jqp-3000.app.github.dev/houston"
+				target=""
+				rel="noopener noreferrer"><img onMouseEnter={handleOnHover}  
+				src="https://townmapsusa.com/images/maps/map_of_houston_tx.jpg" width="250" height="500"
+				className="me-5 cityimage"/></a>
+
+				<button id="backToTopBtn">Back to Top</button>
+			</section>
+
+					 
+				</div>
+				<style>
+				@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap')
+				</style>
+
+			</body>
+				
 		</div>
+
+		
 	);
+
+	
+
+		function handleOnHover(event){
+			console.log("Working on Hover")
+			console.log(event)
+			event.target.style.background="red"
+		}
+
+		function handleOffHover(event){
+			console.log("Working on off Hover")
+		}
+	
+		
 };
--- src/front/js/pages/NYC.js
+++ src/front/js/pages/NYC.js
@@ -7,190 +7,200 @@ export const NYC = () => {
     return (
         <div className="container">
           <div className="restaurant">
-            <i class="fa-regular fa-heart btn btn-danger"></i>
+            <i className="fa-regular fa-heart btn btn-danger me-1
+            "></i>
             <strong>Seasoned Vegan Real Quick</strong>
             <a 
           href="https://www.greenseedvegan.com/"
           target="_blank"
           rel="noopener noreferrer"
-        ><i class="fa-solid fa-shop"></i></a>
+        ><i className="fa-solid fa-shop mx-1"></i></a>
         <br></br>
-            <p><i class="fa-solid fa-face-smile"></i>: 4.4  <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star-half-stroke"></i>(1.8K reviews)</p>
-            <p><i class="fa-solid fa-hand-holding-dollar"></i>: $20–30</p>
-            <p><i class="fa-solid fa-bowl-rice"></i> Vegan</p>
+            <p><i className="fa-solid fa-face-smile"></i>: 4.4  <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-regular fa-star-half-stroke"></i>(1.8K reviews)</p>
+            <p><i className="fa-solid fa-hand-holding-dollar"></i>: $20–30</p>
+            <p><i className="fa-solid fa-bowl-rice"></i> Vegan</p>
             <a 
           href="https://shorturl.at/aemo5"
           target="_blank"
           rel="noopener noreferrer"
-        ><i class="class="fa-solid fa-map-pin></i>
-            <p><i class="fa-solid fa-map-pin"></i> 128 2nd Ave, New York, NY</p></a>
+        ><i className="className="fa-solid fa-map-pin></i>
+            <p><i className="fa-solid fa-map-pin"></i> 128 2nd Ave, New York, NY</p></a>
             <p>Description: Self-serve eatery for organic vegan fare</p>
         
         
-           <i class="fa-regular fa-heart btn btn-danger"></i>
+           <i className="fa-regular fa-heart btn btn-danger me-1
+           "></i>
             <strong>Avant Garden</strong>
             <a 
               href="http://www.avantgardennyc.com/"
               target="_blank"
               rel="noopener noreferrer"
-            ><i class="fa-solid fa-shop"></i></a>
-            <p><i class="fa-solid fa-face-smile"></i>: 4.6<i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star-half-stroke"></i>(644 reviews)</p>
-            <p><i class="fa-solid fa-hand-holding-dollar"></i>: $50–100</p>
-            <p><i class="fa-solid fa-bowl-rice"></i> Vegan</p>
-            <p><i class="fa-solid fa-map-pin"></i> 95 Avenue A, New York, NY</p>
+            ><i className="fa-solid fa-shop"></i></a>
+            <p><i className="fa-solid fa-face-smile"></i>: 4.6<i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-regular fa-star-half-stroke"></i>(644 reviews)</p>
+            <p><i className="fa-solid fa-hand-holding-dollar"></i>: $50–100</p>
+            <p><i className="fa-solid fa-bowl-rice"></i> Vegan</p>
+            <p><i className="fa-solid fa-map-pin"></i> 95 Avenue A, New York, NY</p>
             <p>Description: Might be one of the best vegan places in NYC!</p>
         
-           <i class="fa-regular fa-heart btn btn-danger"></i>
+           <i className="fa-regular fa-heart btn btn-danger me-1
+           "></i>
             <strong>Ladybird</strong>
             <a 
               href="https://shorturl.at/iDOPS"
               target="_blank"
               rel="noopener noreferrer"
-              ><i class="fa-solid fa-shop"></i></a>
+              ><i className="fa-solid fa-shop"></i></a>
               
-            <p><i class="fa-solid fa-face-smile"></i>: 4.5 <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star-half-stroke"></i> (772 reviews)</p>
-            <p><i class="fa-solid fa-hand-holding-dollar"></i>: $30–50</p>
-            <p><i class="fa-solid fa-bowl-rice"></i> Vegan</p>
-            <p><i class="fa-solid fa-map-pin"></i> 111 E 7th St, New York, NY</p>
+            <p><i className="fa-solid fa-face-smile"></i>: 4.5 <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-regular fa-star-half-stroke"></i> (772 reviews)</p>
+            <p><i className="fa-solid fa-hand-holding-dollar"></i>: $30–50</p>
+            <p><i className="fa-solid fa-bowl-rice"></i> Vegan</p>
+            <p><i className="fa-solid fa-map-pin"></i> 111 E 7th St, New York, NY</p>
             <p>Description: Vegetarian tapas in a glamorous room</p>
         
-           <i class="fa-regular fa-heart btn btn-danger"></i>
+           <i className="fa-regular fa-heart btn btn-danger me-1
+           "></i>
             <strong>Willow Vegan Bistro</strong>
             <a 
               href="http://www.avantgardennyc.com/"
               target="_blank"
               rel="noopener noreferrer"
-            ><i class="fa-solid fa-shop"></i></a>
+            ><i className="fa-solid fa-shop"></i></a>
             
-            <p><i class="fa-solid fa-face-smile"></i>: 4.6 <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star-half-stroke"></i> (731 reviews)</p>
-            <p><i class="fa-solid fa-hand-holding-dollar"></i>: $30–50</p>
-            <p><i class="fa-solid fa-bowl-rice"></i> Vegan</p>
-            <p><i class="fa-solid fa-map-pin"></i> 199 8th Ave, New York, NY</p>
+            <p><i className="fa-solid fa-face-smile"></i>: 4.6 <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-regular fa-star-half-stroke"></i> (731 reviews)</p>
+            <p><i className="fa-solid fa-hand-holding-dollar"></i>: $30–50</p>
+            <p><i className="fa-solid fa-bowl-rice"></i> Vegan</p>
+            <p><i className="fa-solid fa-map-pin"></i> 199 8th Ave, New York, NY</p>
             <p>Description: Cozy spot for vegan comfort food</p>
        
-           <i class="fa-regular fa-heart btn btn-danger"></i>
+           <i className="fa-regular fa-heart btn btn-danger me-1
+           "></i>
             <strong>Planta Queen</strong>
             <a 
               href="https://www.plantarestaurants.com/location/planta-queen-new-york-city/"
               target="_blank"
               rel="noopener noreferrer"
-            ><i class="fa-solid fa-shop"></i></a>
+            ><i className="fa-solid fa-shop"></i></a>
             
-            <p><i class="fa-solid fa-face-smile"></i>: 4.6 <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star-half-stroke"></i> (1.7K reviews)</p>
-            <p><i class="fa-solid fa-hand-holding-dollar"></i>: $50–100</p>
-            <p><i class="fa-solid fa-bowl-rice"></i> Vegan</p>
-            <p><i class="fa-solid fa-map-pin"></i> 15 W 27th St, New York, NY</p>
+            <p><i className="fa-solid fa-face-smile"></i>: 4.6 <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-regular fa-star-half-stroke"></i> (1.7K reviews)</p>
+            <p><i className="fa-solid fa-hand-holding-dollar"></i>: $50–100</p>
+            <p><i className="fa-solid fa-bowl-rice"></i> Vegan</p>
+            <p><i className="fa-solid fa-map-pin"></i> 15 W 27th St, New York, NY</p>
             <p>Description: Dine-in, Takeout, Delivery</p>
         
-           <i class="fa-regular fa-heart btn btn-danger"></i>
+           <i className="fa-regular fa-heart btn btn-danger me-1
+           "></i>
             <strong>Le Botaniste</strong>
             <a 
               href="http://lebotaniste.us/"
               target="_blank"
               rel="noopener noreferrer"
-            ><i class="fa-solid fa-shop"></i></a>
+            ><i className="fa-solid fa-shop"></i></a>
             
-            <p><i class="fa-solid fa-face-smile"></i>: 4.7  <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star-half-stroke"></i>(943 reviews)</p>
-            <p><i class="fa-solid fa-hand-holding-dollar"></i>: $10–20</p>
-            <p><i class="fa-solid fa-bowl-rice"></i> Vegan</p>
-            <p><i class="fa-solid fa-map-pin"></i> 833 Lexington Ave, New York, NY</p>
+            <p><i className="fa-solid fa-face-smile"></i>: 4.7  <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-regular fa-star-half-stroke"></i>(943 reviews)</p>
+            <p><i className="fa-solid fa-hand-holding-dollar"></i>: $10–20</p>
+            <p><i className="fa-solid fa-bowl-rice"></i> Vegan</p>
+            <p><i className="fa-solid fa-map-pin"></i> 833 Lexington Ave, New York, NY</p>
             <p>Description: Quirky counter-serve vegan cafe and wine bar</p>
        
-           <i class="fa-regular fa-heart btn btn-danger"></i>
+           <i className="fa-regular fa-heart btn btn-danger me-1
+           "></i>
             <strong>Dirt Candy</strong>
             <a 
               href="http://www.dirtcandynyc.com/"
               target="_blank"
               rel="noopener noreferrer"
-            ><i class="fa-solid fa-shop"></i></a>
+            ><i className="fa-solid fa-shop"></i></a>
             
-            <p><i class="fa-solid fa-face-smile"></i>: 4.6  <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star-half-stroke"></i>(1.2K reviews)</p>
-            <p><i class="fa-solid fa-hand-holding-dollar"></i>: $100+</p>
-            <p><i class="fa-solid fa-bowl-rice"></i> Vegan</p>
-            <p><i class="fa-solid fa-map-pin"></i> 86 Allen St, New York, NY</p>
+            <p><i className="fa-solid fa-face-smile"></i>: 4.6  <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-regular fa-star-half-stroke"></i>(1.2K reviews)</p>
+            <p><i className="fa-solid fa-hand-holding-dollar"></i>: $100+</p>
+            <p><i className="fa-solid fa-bowl-rice"></i> Vegan</p>
+            <p><i className="fa-solid fa-map-pin"></i> 86 Allen St, New York, NY</p>
             <p>Description: I would give this restaurant 10 stars out of 5 if I could!</p>
        
-           <i class="fa-regular fa-heart btn btn-danger"></i>
+           <i className="fa-regular fa-heart btn btn-danger me-1
+           "></i>
             <strong>Vegan On The Fly</strong>
             <a 
               href="http://www.veganontf.com/"
               target="_blank"
               rel="noopener noreferrer"
-            ><i class="fa-solid fa-shop"></i></a>
+            ><i className="fa-solid fa-shop"></i></a>
             
-            <p><i class="fa-solid fa-face-smile"></i>: 4.8  <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star-half-stroke"></i>(945 reviews)</p>
-            <p><i class="fa-solid fa-hand-holding-dollar"></i>: $10–20</p>
-            <p><i class="fa-solid fa-bowl-rice"></i> Vegan</p>
-            <p><i class="fa-solid fa-map-pin"></i> 24 W 45th St, New York, NY</p>
+            <p><i className="fa-solid fa-face-smile"></i>: 4.8  <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-regular fa-star-half-stroke"></i>(945 reviews)</p>
+            <p><i className="fa-solid fa-hand-holding-dollar"></i>: $10–20</p>
+            <p><i className="fa-solid fa-bowl-rice"></i> Vegan</p>
+            <p><i className="fa-solid fa-map-pin"></i> 24 W 45th St, New York, NY</p>
             <p>Description: Easygoing eatery for plant-based fare</p>
         
-           <i class="fa-regular fa-heart btn btn-danger"></i>
+           <i className="fa-regular fa-heart btn btn-danger me-1
+           "></i>
             <strong>Blossom on Columbus</strong>
             <a 
               href="https://www.blossomnyc.com/"
               target="_blank"
               rel="noopener noreferrer"
-            ><i class="fa-solid fa-shop"></i></a>
+            ><i className="fa-solid fa-shop"></i></a>
             
-            <p><i class="fa-solid fa-face-smile"></i>: 4.4  <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star-half-stroke"></i>(1.1K reviews)</p>
+            <p><i className="fa-solid fa-face-smile"></i>: 4.4  <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-regular fa-star-half-stroke"></i>(1.1K reviews)</p>
             
-            <p><i class="fa-solid fa-hand-holding-dollar"></i>: $30–50</p>
-            <p><i class="fa-solid fa-bowl-rice"></i> Vegan</p>
-            <p><i class="fa-solid fa-map-pin"></i> 507 Columbus Ave, New York, NY</p>
+            <p><i className="fa-solid fa-hand-holding-dollar"></i>: $30–50</p>
+            <p><i className="fa-solid fa-bowl-rice"></i> Vegan</p>
+            <p><i className="fa-solid fa-map-pin"></i> 507 Columbus Ave, New York, NY</p>
             <p>Description: Upscale organic vegan restaurant</p>
         
-           <i class="fa-regular fa-heart btn btn-danger"></i>
+           <i className="fa-regular fa-heart btn btn-danger me-1
+           "></i>
             <strong>Delice & Sarrasin</strong>
             <a 
               href="http://delicesarrasin.com/"
               target="_blank"
               rel="noopener noreferrer"
-            ><i class="fa-solid fa-shop"></i></a>
+            ><i className="fa-solid fa-shop"></i></a>
             
-            <p><i class="fa-solid fa-face-smile"></i>: 4.5  <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star-half-stroke"></i>(985 reviews)</p>
-            <p><i class="fa-solid fa-hand-holding-dollar"></i>: $30–50</p>
-            <p><i class="fa-solid fa-bowl-rice"></i> Vegan</p>
-            <p><i class="fa-solid fa-map-pin"></i> 178 W Houston St, New York, NY</p>
+            <p><i className="fa-solid fa-face-smile"></i>: 4.5  <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-solid fa-star"></i>
+        <i className="fa-regular fa-star-half-stroke"></i>(985 reviews)</p>
+            <p><i className="fa-solid fa-hand-holding-dollar"></i>: $30–50</p>
+            <p><i className="fa-solid fa-bowl-rice"></i> Vegan</p>
+            <p><i className="fa-solid fa-map-pin"></i> 178 W Houston St, New York, NY</p>
             <p>Description: Cafe with crêpes & French vegan dishes</p>
       
         
