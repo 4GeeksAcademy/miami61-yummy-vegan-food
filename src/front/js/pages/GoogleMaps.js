caa0d966ab90c4d5f84dc42252b081850de343ad
Jaemarlee Goin <150928570+Jaemarlee@users.noreply.github.com>
2024-04-10 23:19:35 +0000

    Building google maps

--- src/front/js/pages/GoogleMaps.js
+++ src/front/js/pages/GoogleMaps.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
+export class GoogleMaps extends Component {
+    componentDidMount() {
+        console.log(">>> google maps:", this.props.google);
+     }
+     componentDidUpdate (prevProps, prevState) {
+        console.log(">>> did update:", prevProps, prevState, this.props.google);
+    };
+  render() {
+    const mapStyles = {
+      width: '100%',
+      height: '60vh'
+    };
 
-export const GoogleMaps = () => {
-
-	return (
-		<div className="container d-flex flex-column align-items-center">
-            <h1 className="mt-4">Google Maps API</h1>
-            <p>Admasu's got this! =)</p>
-        </div>
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+          lat: 34.0522,
+          lng: -118.2437
+        }}
+      >
+        <Marker position={{ lat: 34.0522, lng: -118.2437 }} />
+      </Map>
     );
-};
+  }
+}
+const LoadingContainer = (props) => (
+    <div>Fancy loading container!</div>
+  )
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBWpLg3Y4oQxsbUyCgYfQh98SwPQlt263Q',
+  LoadingContainer:LoadingContainer // Replace with your actual API key
+})(GoogleMaps);
